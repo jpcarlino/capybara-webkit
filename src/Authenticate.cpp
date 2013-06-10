c5a5e25b037a14850e4080719d10572426a197f3
Juan Pablo Carlino <jpcarlino@gmail.com>
2013-06-10 17:58:52 -0300

    QtNetwork.h -> QtNetwork

--- src/Authenticate.cpp
+++ src/Authenticate.cpp
@@ -1,7 +1,7 @@
 #include "Authenticate.h"
 #include "WebPage.h"
 #include "NetworkAccessManager.h"
-#include "QtNetwork.h"
+#include "QtNetwork"
 
 Authenticate::Authenticate(WebPageManager *manager, QStringList &arguments, QObject *parent) : SocketCommand(manager, arguments, parent) {
 }
