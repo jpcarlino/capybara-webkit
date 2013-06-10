5556f05dc29479eae50da4f4b6aea70bc7849f9b
Juan Pablo Carlino <jpcarlino@gmail.com>
2013-06-10 17:29:30 -0300

    fix include directive to support qtwebkit >= 2.3.0

--- src/Authenticate.cpp
+++ src/Authenticate.cpp
@@ -1,6 +1,7 @@
 #include "Authenticate.h"
 #include "WebPage.h"
 #include "NetworkAccessManager.h"
+#include "QtNetwork.h"
 
 Authenticate::Authenticate(WebPageManager *manager, QStringList &arguments, QObject *parent) : SocketCommand(manager, arguments, parent) {
 }
